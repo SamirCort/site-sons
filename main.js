9783828213ad0ae11b802983f1fd5eda1ae3fe76
SamirCort <127320813+SamirCort@users.noreply.github.com>
2023-11-29 11:57:25 -0300

    a

--- main.js
+++ main.js
@@ -5,13 +5,10 @@ function tocaSom(idElemento){
 }
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length;contador++){
     const instrumento = listaDeTeclas[contador].classList
     console.log(instrumento[1])
     listaDeTeclas[contador].onclick = function(){
         tocaSom(`#som_${instrumento[1]}`);
     }
-    contador = contador + 1;
 }
